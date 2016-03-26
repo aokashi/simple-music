81bc2af5954d0fa50a44fdb681ad1be45883ecf7
Aokashi <Aokashi.Towa@Gmail.com>
2016-03-26 16:27:47 +0900

    音楽関連で色々強化
    10秒進めたり、早さや音量を調整したり

--- app/js/app.js
+++ app/js/app.js
@@ -8,33 +8,61 @@ function installServiceWorker(){
   });
 }
 
-function playMusic(){
+function toggleMusic(){
   console.log("再生ぼたんがおされたよ");
   var player = document.querySelector("#player");
-  var button = document.querySelector("#playButton");
+  var button = document.querySelector("#toggleButton");
   if(player.paused){
     player.play();
-    button.innerHTML = "停止！";
+    button.innerHTML = "一時停止！";
   }else{
     player.pause();
-    button.innerHTML = "再生！";
+    button.innerHTML = "　　再生！";
   }
+};
+
+function stopMusic(){
+  console.log("停止ボタンが押されたよ");
+  var player = document.querySelector("#player");
+  var button = document.querySelector("#toggleButton");
+  player.currentTime = 0;
+  player.pause();
+  button.innerHTML = "　　再生！";
 };
 
 function setMusic(){
   console.log("設定ぼたんがおされたよ");
   var player = document.querySelector("#player");
-  player.currentTime = 100;
-}
+  player.currentTime += 10;
+};
+
+function boostMusic(){
+  console.log("倍速ボタンがおされたよ");
+  var player = document.querySelector("#player");
+  var slider = document.querySelector("#boost");
+  player.playbackRate = slider.value;
+};
+
+function changeVolume(){
+  var player = document.querySelector("#player");
+  var slider = document.querySelector("#volume");
+  player.volume = slider.value;
+};
 
 function init(){
   var player = document.querySelector("#player");
   console.log(player);
   
-  var playButton = document.querySelector("#playButton");
-  playButton.addEventListener("click", playMusic);
+  var toggleButton = document.querySelector("#toggleButton");
+  toggleButton.addEventListener("click", toggleMusic);
+  var stopButton = document.querySelector("#stopButton");
+  stopButton.addEventListener("click", stopMusic);
   var setButton = document.querySelector("#setButton");
   setButton.addEventListener("click", setMusic);
+  var boostSlider = document.querySelector("#boost");
+  boostSlider.addEventListener("change", boostMusic);
+  var volumeSlider = document.querySelector("#volume");
+  volumeSlider.addEventListener("change", changeVolume);
 };
 
 window.addEventListener("load", event =>{
